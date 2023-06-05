3bdb62bf2d9716d5c9707d9b336adfd0d76d21a7
sceptic <galaxyasamsung979@gmail.com>
2023-06-05 17:51:03 +0500

    modified CategorySection file

--- app/Http/Livewire/CategorySection.php
+++ app/Http/Livewire/CategorySection.php
@@ -16,78 +16,7 @@ class CategorySection extends Component
 
     public $parentCategories;
 
-    public function mount()
-    {
-        $this->parentCategories = Category::query()->orderByDesc('id')->get();
-    }
-
-    public function resetForm()
-    {
-        $this->parent_id = '';
-        $this->name = '';
-        $this->description = '';
-    }
-
-    public function addCategory()
-    {
-        $this->resetForm();
-        $this->emit('open-create-modal');
-    }
-
-    public function store()
-    {
-        $this->validate([
-            'name' => 'required',
-            'parent_id' => 'required|exists:categories,id'
-        ]);
-
-        Category::create([
-            'name' => $this->name,
-            'description' => $this->description,
-            'parent_id' => $this->parent_id
-        ]);
-
-        $this->dispatchBrowserEvent('created', ['message' => 'category created successfully!']);
-
-        $this->resetForm();
-        $this->mount();
-    }
-
-
-    public function edit(int $id)
-    {
-        $this->resetForm();
-
-        $category = Category::findOrFail($id);
-        $this->category_id = $category->id;
-        $this->name = $category->name;
-        $this->description = $category->description;
-        $this->parent_id = $category->parent_id;
-
-        $this->emit('open-edit-modal');
-    }
-
-
-    public function update()
-    {
-        $this->validate([
-            'name' => 'required',
-            'parent_id' => 'required|exists:categories,id'
-        ]);
-
-        $category = Category::find($this->category_id);
-       
-        $category->update([
-            'name' => $this->name,
-            'description' => $this->description,
-            'parent_id' => $this->parent_id
-        ]);
-
     
-        $this->dispatchBrowserEvent('updated', ['message' => 'category updated successfully']);
-        $this->resetForm();
-        
-    }
 
 
     public function deleteConfirm(int $id)
